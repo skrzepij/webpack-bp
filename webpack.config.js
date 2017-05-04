689b6f7bf20ce87aa279d5be72246ae6791a9c95
Radomir Skrzepij <skrzepij@gmail.com>
2017-05-04 20:43:01 +0200

    podpiecie postcss i dzialajacy SASS

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,66 @@
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var webpack = require('webpack');
+var path = require('path');
 var root = __dirname;
 
 module.exports = {
-    entry: "./entry.js",
-    output: {
-        path: 'build',
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [{
-          test: /\.scss$/,
-          // use: [
-          //   { loader: "style-loader" },
-          //   { loader: "css-loader" }
-          // ]
-          use: ExtractTextPlugin.extract({
-            fallback: "style-loader",
-            //resolve-url-loader may be chained before sass-loader if necessary
-            use: ['css-loader', 'sass-loader']
-          })
-        }]
-    },
+  context: root,
+  entry: {
+    bundle: "./entry.js"
+  },
+  output: {
+      path: path.join(root, 'build'),
+      filename: "[name].js"
+  },
+  module: {
+      rules: [{
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [{
+            loader: 'css-loader', // translates CSS into CommonJS
+            options: {
+              minimize: true,
+              importLoaders: 1,
+            }
+          },
+          {
+            loader: 'postcss-loader' // postcss.config.js
+          },
+          {
+            loader: 'sass-loader', // compiles Sass to CSS
+            options: {
+              // data: "$env: " + process.env.NODE_ENV + ";"
+            }
+          }],
+          //resolve-url-loader may be chained before sass-loader if necessary
+          fallback: "style-loader",
+        })
+      }//,
+    //   {
+    //   test: /\.css$/,
+    //   exclude: /node_modules/,
+    //   use: [
+    //       {
+    //           loader: 'style-loader',
+    //       },
+    //       {
+    //           loader: 'css-loader',
+    //           options: {
+    //               importLoaders: 1,
+    //           }
+    //       },
+    //       {
+    //           loader: 'postcss-loader'
+    //       }
+    //   ]
+    // }
+    ]
+  },
   plugins: [
-    new ExtractTextPlugin("styles.css"),
+    new ExtractTextPlugin({
+      filename: "styles.css"
+      //    filename: "[name].[contenthash].css"
+      //     disable: process.env.NODE_ENV === "development"
+    })
   ]
 };
-
-
-// const path = require('path');
-// const webpack = require('webpack');
-// const ExtractTextPlugin = require("extract-text-webpack-plugin");
-// const extractSass = new ExtractTextPlugin({
-//     filename: "[name].[contenthash].css",
-//     disable: process.env.NODE_ENV === "development"
-// });
-//
-// module.exports = {
-//     module: {
-//         rules: [{
-//             test: /\.scss$/,
-//             use: extractSass.extract({
-//                 use: [{
-//                     loader: "css-loader" // translates CSS into CommonJS
-//                 }, {
-//                     loader: "sass-loader" // compiles Sass to CSS
-//                 }],
-//                 // use style-loader in development
-//                 fallback: "style-loader"
-//             })
-//         }]
-//     },
-//     plugins: [
-//         extractSass
-//     ]
-// };
